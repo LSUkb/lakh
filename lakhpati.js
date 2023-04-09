db40e5e9f0a730d616113e0b8804b869129fc298
LSUkb <127669584+LSUkb@users.noreply.github.com>
2023-04-10 03:49:04 +0530

    Delete lakhpati.js

--- lakhpati.js
+++
@@ -1,8 +0,0 @@
-function myFunction(){
-    if(document.getElementById("sideNav").style.left=="1000px"){document.getElementById("sideNav").style.left="500px";
-  document.getElementById("menu").style.left="1000px";
-    document.getElementById("close").style.left="845px";}else{document.getElementById("sideNav").style.left="1000px";
-    document.getElementById("menu").style.left="845px";
-    document.getElementById("close").style.left="1000px";}
-   
-}
